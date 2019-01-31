4e498f36f83cdf9955ea46d32338b0e694eb13dd
purplesands <mullenjoe13@gmail.com>
2019-01-30 19:31:12 -0500

    fixed slug

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,22 +3,26 @@ has_many :song_genres
 has_many :genres, through: :song_genres
 belongs_to :artist
 
-def slug
-  self.name.downcase.gsub(' ', '-')
-end
-
-  def self.find_by_slug_helper(slug)
-    slug = slug.split("-")
-    slug.map do |word|
-      word.capitalize
-    end.join(" ")
+  def slug
+    self.name.downcase.gsub(' ', '-')
   end
+
+  # def self.find_by_slug_helper(slug)
+  #   slug = slug.split("-")
+  #   slug.map do |word|
+  #     word.capitalize
+  #   end.join(" ")
+  # end
+
+  # def self.find_by_slug(slug)
+  #   formatted = Song.find_by_slug_helper(slug)
+  #   # binding.pry
+  #   Song.find_by_name(formatted)
+  # end
 
   def self.find_by_slug(slug)
-    formatted = Song.find_by_slug_helper(slug)
-    # binding.pry
-    Song.find_by_name(formatted)
+  @song = Song.find do |song|
+    song.slug == slug
+    end
   end
-
-
 end
